6df3faa9c866ca880a2852ba41498f1b163c4cde
username <user@email.mail>
2021-03-28 15:29:21 +0900

    환경세팅 완료

---
+++ src/test/java/com/kjs/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kjs.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
